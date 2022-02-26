5334b10e0dcc4fb0d97222e7b665b1d7f3b135c2
Kozzztia <kozzztia@ukr.net>
2022-02-27 01:22:39 +0200

    slider27.02

--- app/js/main.js
+++ app/js/main.js
@@ -5,40 +5,59 @@
 
 
 
-// swiper
+// firstSlider
+const firstItems = document.querySelectorAll('.firstSlider__item')
+const firstPrev = document.querySelector('.firstSlider__btn--prev');
+const firstNext = document.querySelector('.firstSlider__btn--next');
 
 
-new Swiper('.first-slider', {
-    // buttons
-    navigation: {
-        nextEl: '.first-button-next',
-        prevEl: '.first-button-prev',
-    },
-    // bullets
-    pagination: {
-        el: '.first-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
-    // progressbar
-    // type:'progressbar'
-    // type:'fraction'
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    //     bragable: true,
-    // }
-    // slideToClickedSlide: true,
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown: true,
-    },
-    mousewheel: {
-        sensitivity: 1,
-        // eventsTarget: '.first-slider',
-    },
-    // number of sliders
-    sliderPreView: 3,
-    loop: true,
 
-})
+function sliderMove(item, prev, next, prevClass, nextClass, activeClass) {
+    let left = 0;
+    let right = 2;
+    let active = 1;
+    function deletClass() {
+        for (i = 0; i < item.length; i++) {
+            item[i].classList.remove(prevClass);
+            item[i].classList.remove(nextClass);
+            item[i].classList.remove(activeClass);
+        }
+    }
+    next.onclick = function () {
+        left++;
+        right++;
+        active++;
+        deletClass()
+        item[left].classList.add('firstSlider__item--prev');
+        item[right].classList.add('firstSlider__item--next');
+        item[active].classList.add('firstSlider__item--active');
+        console.log(active)
+        if (left === item.length - 1) {
+            left = -1;
+        } else if (right === item.length - 1) {
+            right = -1;
+        } else if (active === item.length - 1) {
+            active = -1;
+        }
+    }
+    prev.onclick = function () {
+        if (left <= 0) {
+            left = item.length;
+        } else if (right === 0) {
+            right = item.length;
+        } else if (active === 0) {
+            active = item.length;
+        }
+        left--;
+        right--;
+        active--;
+        deletClass()
+        item[left].classList.add('firstSlider__item--prev');
+        item[right].classList.add('firstSlider__item--next');
+        item[active].classList.add('firstSlider__item--active');
+        console.log(active)
+    }
+
+
+}
+sliderMove(firstItems, firstPrev, firstNext, 'firstSlider__item--prev', 'firstSlider__item--next', 'firstSlider__item--active') 
